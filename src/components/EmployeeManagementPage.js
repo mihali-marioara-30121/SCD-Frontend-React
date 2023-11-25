0dce85bd13421f91c706d4e7965361d2e38e3c52
mihali-marioara-30121 <marioaramihali2002@gmail.com>
2023-11-25 18:14:54 +0200

    gataa

--- src/components/EmployeeManagementPage.js
+++ src/components/EmployeeManagementPage.js
@@ -1,12 +1,299 @@
 // components/EmployeeManagementPage.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Box,
+  Typography,
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Checkbox,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Snackbar,
+} from '@mui/material';
 
 const EmployeeManagementPage = () => {
+  const [departments, setDepartments] = useState([]);
+  const [selectedDepartment, setSelectedDepartment] = useState(null);
+  const [employees, setEmployees] = useState([]);
+  const [managers, setManagers] = useState([]);
+  const [selectedEmployees, setSelectedEmployees] = useState([]);
+  const [selectedManagers, setSelectedManagers] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [emailSubject, setEmailSubject] = useState('');
+  const [emailContent, setEmailContent] = useState('');
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+
+  // Fetch departments on component mount
+  useEffect(() => {
+    const fetchDepartments = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/v1/department/all', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+          },
+        });
+        setDepartments(response.data || []);
+      } catch (error) {
+        console.error('Error fetching departments:', error);
+      }
+    };
+
+    fetchDepartments();
+  }, []);
+
+  // Fetch employees and managers when the selected department changes
+  useEffect(() => {
+    const fetchEmployeesAndManagers = async () => {
+      if (selectedDepartment) {
+        try {
+          const responseEmployees = await axios.get(
+            `http://localhost:8080/api/v1/employee/byDepartament/employees/${selectedDepartment.name}`,
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+              },
+            }
+          );
+          setEmployees(responseEmployees.data || []);
+
+          const responseManagers = await axios.get(
+            `http://localhost:8080/api/v1/employee/byDepartament/managers/${selectedDepartment.name}`,
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+              },
+            }
+          );
+          setManagers(responseManagers.data || []);
+        } catch (error) {
+          console.error('Error fetching employees and managers:', error);
+        }
+      }
+    };
+
+    fetchEmployeesAndManagers();
+  }, [selectedDepartment]);
+
+  const handleCheckboxChange = (type, employee) => {
+    if (type === 'employee') {
+      setSelectedEmployees((prev) =>
+        prev.includes(employee.id)
+          ? prev.filter((id) => id !== employee.id)
+          : [...prev, employee.id]
+      );
+    } else {
+      setSelectedManagers((prev) =>
+        prev.includes(employee.id)
+          ? prev.filter((id) => id !== employee.id)
+          : [...prev, employee.id]
+      );
+    }
+  };
+
+  const handleSendEmail = () => {
+    setOpenDialog(true);
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+  };
+
+  const handleConfirmSendEmail = async () => {
+    try {
+      if (selectedEmployees.length === 0 && selectedManagers.length === 0) {
+        setSnackbarMessage('Please select at least one employee or manager.');
+        setOpenSnackbar(true);
+        return;
+      }
+
+      if (!emailSubject || !emailContent) {
+        setSnackbarMessage('Subject and content cannot be empty.');
+        setOpenSnackbar(true);
+        return;
+      }
+
+      // Send email to selected employees
+      await axios.post(
+        'http://localhost:8080/api/v1/email',
+        {
+          subject: emailSubject,
+          message: emailContent,
+          recipientDetailsList: [...selectedEmployees, ...selectedManagers].map((id) => {
+            const employee = [...employees, ...managers].find(
+              (emp) => emp.id === id
+            );
+            return {
+              name: employee ? employee.name : 'Unknown Employee/Manager',
+              email: employee ? employee.email : '',
+            };
+          }),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+          },
+        }
+      );
+
+      // Clear selected employees and close the dialog after sending email
+      setSelectedEmployees([]);
+      setSelectedManagers([]);
+      setEmailSubject('');
+      setEmailContent('');
+      setOpenDialog(false);
+
+      setSnackbarMessage('Email sent successfully.');
+      setOpenSnackbar(true);
+    } catch (error) {
+      console.error('Error sending email:', error);
+      setSnackbarMessage('Sending email failed.');
+      setOpenSnackbar(true);
+    }
+  };
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenSnackbar(false);
+  };
+
   return (
-    <div>
-      <h1>Employee Management Page</h1>
-      <p>This is a dummy page for employee management. Add your real content and components here.</p>
-    </div>
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <FormControl fullWidth style={{ width: 100, margin: '30px 0' }}>
+        <InputLabel htmlFor="department-select">Select Department</InputLabel>
+        <Select
+          label="Select Department"
+          value={selectedDepartment ? selectedDepartment.name : ''}
+          onChange={(e) => {
+            const selected = departments.find(
+              (dept) => dept.name === e.target.value
+            );
+            setSelectedDepartment(selected);
+          }}
+          inputProps={{ name: 'department', id: 'department-select' }}
+        >
+          {departments.map((dept) => (
+            <MenuItem key={dept.id} value={dept.name}>
+              {dept.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+      {selectedDepartment && (
+        <Box mt={2} width="80%">
+          <Typography variant="h5">Employees and Managers in {selectedDepartment.name}</Typography>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Manager</TableCell>
+                  <TableCell>Select</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {[...employees, ...managers].map((employee) => (
+                  <TableRow key={employee.id}>
+                    <TableCell>{employee.name}</TableCell>
+                    <TableCell>{employee.email}</TableCell>
+                    <TableCell>{employee.manager ? employee.manager.name : 'N/A'}</TableCell>
+                    <TableCell>
+                      <Checkbox
+                        checked={
+                          (employee.manager && selectedManagers.includes(employee.id)) ||
+                          (!employee.manager && selectedEmployees.includes(employee.id))
+                        }
+                        onChange={() =>
+                          handleCheckboxChange(
+                            employee.manager ? 'manager' : 'employee',
+                            employee
+                          )
+                        }
+                      />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          <Button variant="contained" color="primary" onClick={handleSendEmail}>
+            Send Email
+          </Button>
+        </Box>
+      )}
+
+      <Dialog open={openDialog} onClose={handleDialogClose}>
+        <DialogTitle>Send Email</DialogTitle>
+        <DialogContent>
+          <Typography variant="subtitle1">Selected Employees/Managers:</Typography>
+          <ul>
+            {[...selectedEmployees, ...selectedManagers].map((id) => {
+              const employee = [...employees, ...managers].find(
+                (emp) => emp.id === id
+              );
+              return (
+                <li key={id}>
+                  {employee ? employee.name : 'Unknown Employee/Manager'}
+                </li>
+              );
+            })}
+          </ul>
+          <TextField
+            label="Subject"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={emailSubject}
+            onChange={(e) => setEmailSubject(e.target.value)}
+          />
+          <TextField
+            label="Content"
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={4}
+            margin="normal"
+            value={emailContent}
+            onChange={(e) => setEmailContent(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirmSendEmail} color="primary">
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        message={snackbarMessage}
+      />
+    </Box>
   );
 };
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,72 +1,107 @@
-// components/LoginPage.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import extractRoleFromToken from '../services/JwtDecoder';
-import { useNavigate  } from 'react-router-dom'; // Import useHistory
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField, Container, CssBaseline, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+
+const useStyles = styled((theme) => ({
+  paper: {
+    marginTop: theme.spacing(80),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center', // Add this line
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const LoginPage = () => {
+  const classes = useStyles();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate (); // Initialize history
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const requestBody = {
-        "username": username,
-        "password": password
-      }
-       const result = await axios.post("http://localhost:8080/api/v1/users/login", requestBody);
-       const jwt = result?.data?.token;
-       localStorage.setItem("jwt", jwt);
-
+        username: username,
+        password: password,
+      };
+      const result = await axios.post("http://localhost:8080/api/v1/users/login", requestBody);
+      const jwt = result?.data?.token;
+      localStorage.setItem("jwt", jwt);
 
       const token = result?.data?.token;
       const role = extractRoleFromToken(token);
 
       localStorage.setItem("jwt", jwt);
- 
+
       if (role === "DIRECTOR") {
-        // Redirect catre pagina de vizualizare a angajatilor
+        // Redirect to the employee management page
         console.log('ROLE IS DIRECTOR ---- OK');
         navigate('/employeeManagement');
-
       } else {
-        // afisare mesaj eroare
+        // Display error message
       }
-      
 
-      // Now you can use the 'role' in your application logic
       console.log('User Role:', role);
     } catch (error) {
       console.error('Error logging in:', error);
     }
   };
 
-
-
   return (
-    <div>
-      <h1>Login</h1>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+<Container component="main" maxWidth="xs" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+        </form>
       </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    </Container>
   );
 };
 
